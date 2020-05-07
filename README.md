774588ab62073f53add89219119ac78dc05dffd2
ZaoHan415 <hanzeyao415@gmail.com>
2020-05-07 22:17:38 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sessda2048
  
+## 使用
+
+- 把技术组写的那个[sessdsa.2048](https://github.com/pkulab409/sessdsa.2048)整个拷到本地
+- 修改variables.py里的路径
+- 直接运行main.py
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import analyser as gsm
 import round_match as rm
 
 
-player1 = r'./playerA.py'
-player2 = r'./playerB.py'
+player1 = r'./player_Random.py'
+player2 = r'./player_Random.py'
+# 大战10回合
 a = rm.main([player1, player2])
+# 调用技术组的复盘程序
 gameScreen = gsm.GameScreen()
