27d47e724ebabe73afc1020e11980a8fad7bec92
ZaoHan415 <hanzeyao415@gmail.com>
2020-05-07 22:36:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import variables
 import analyser as gsm
-import round_match as rm
+import neattool as nt
+import time
 
 
 player1 = r'./player_Random.py'
 player2 = r'./player_Random.py'
-# 大战10回合
-a = rm.main([player1, player2])
-# 调用技术组的复盘程序
-gameScreen = gsm.GameScreen()
+foldername = time.strftime('%Y-%m-%d %H-%M-%S', time.localtime())
+# 大战10回合，会自动调用技术组的复盘程序
+a = nt.main([player1, player2], savepath=foldername)
