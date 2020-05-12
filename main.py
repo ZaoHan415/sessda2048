b4f60bc75a58645fa1eed1e1b9e8ec3c3951b375
ZaoHan415 <hanzeyao415@gmail.com>
2020-05-12 19:43:13 +0800

    update alpha-beta

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import variables
 import round_match as rm
-import analyser as gsm
-import neattool as nt
+# import analyser as gsm
+# import neattool as nt
 import time
 
 
-player1 = r'./player_Minmax_StableVer.py'
+player1 = r'./src/player_Minmax_PreviewVer.py'
 # player2 = r'./player_Random.py'
-player2 = r'./player_Minmax_PreviewVer.py'
+player2 = r'./src/thanos.py'
 foldername = time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime())
 # 大战10回合，会自动调用技术组的复盘程序
-a = nt.main([player1, player2], savepath=foldername, REPEAT=5, MAXTIME=10)
+a = rm.main([player1, player2], savepath=foldername, debug=True, REPEAT=10, MAXTIME=10)
