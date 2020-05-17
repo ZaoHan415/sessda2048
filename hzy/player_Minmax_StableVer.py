0e1e92ba322854b549e98fec04208b03c3f11e03
ZaoHan415 <hanzeyao415@gmail.com>
2020-05-17 12:41:12 +0800

    stable对应小萝莉

--- hzy/player_Minmax_StableVer.py
+++ hzy/player_Minmax_StableVer.py
@@ -1,6 +1,7 @@
 # 基础版minmax 算法
 # 创建于2020年5月8日 by Czarja
 # 添加αβ剪枝，2020 5 09 by DXLi
+import numpy
 
 
 class Player:
@@ -9,6 +10,12 @@ class Player:
         self.isFirst = isFirst
         self.array = array
         self.maxValue = 2E9
+        # self.weight = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+        self.weight = [1, 2, 4, 8, 16, 32, 64, 128, 259, 519, 1039, 2079, 4159]
+        # self.weight = [1, 2, 5, 11, 23, 47, 95, 191, 383, 767, 1535, 3071, 6143]
+        # self.weight = [1, 3, 7, 15, 31, 63, 127, 255, 511, 1023, 2047, 4095, 8191]
+        self.maxRounds = len(array)
+        self.totalTime = 5
 
     @staticmethod
     def cannotMove(belong, board):  # 如果某方无路可走返回True
@@ -29,41 +36,37 @@ class Player:
         else:
             return [0, 1, 2, 3]
 
-    @staticmethod
-    def weightSum(lst):  # 平方和作为总分
-        total = 0
-        for a in lst:
-            total += a**2
-        return total
+    def weightSum(self, lst):  # 平方和作为总分
+        return sum(map(self.weight.__getitem__, lst))
 
     def score(self, board):  # 最简单的局面估值函数
         # 目前完全是玄学估值
         myScoreLst = board.getScore(self.isFirst)
         RivalScoreLst = board.getScore(not self.isFirst)
-        total = sum(myScoreLst) - Player.weightSum(RivalScoreLst)/2
+        total = self.weightSum(myScoreLst) - self.weightSum(RivalScoreLst)/2
         # 一个空位价值1.5分
         total += len(board.getNone(self.isFirst))*1.5
         # 无路可走的情况要避免
-        if Player.cannotMove(self.isFirst, board):
-            return -self.maxValue
-        elif Player.cannotMove(not self.isFirst, board):
-            return self.maxValue
-        else:
-            return total
+        # if Player.cannotMove(self.isFirst, board):
+        #     return -self.maxValue
+        # elif Player.cannotMove(not self.isFirst, board):
+        #     return self.maxValue
+        # else:
+        return total
 
     def _minMaxRecur(self, board, depth, phase, currentRound, inc, alpha=-(2E9), beta=+(2E9)):
         # 返回值为局面估值
-        if depth <= 0:
+        if depth <= 0 or currentRound >= self.maxRounds:
             return self.score(board)
         peer = not bool(phase % 2)
         flag = bool(peer == self.isFirst)
         if phase == 2 or phase == 3:
             if phase == 3:
                 currentRound += 1
-            # if currentRound > 150:
-            #     if inc < 4:
-            #         depth += 1
-            #         inc += 1
+            if currentRound > 100:
+                tLeft = board.getTime(self.isFirst)
+                if (self.totalTime - tLeft) * self.maxRounds / currentRound > self.totalTime:
+                    depth -= 1
             for d in [0, 1, 2, 3]:
                 newBoard = board.copy()
                 if newBoard.move(peer, d):
@@ -72,17 +75,15 @@ class Player:
                         beta = curScore
                         if alpha >= beta:
                             return alpha
-                    if flag and (curScore > alpha):
+                    elif flag and (curScore > alpha):
                         alpha = curScore
                         if alpha >= beta:
                             return beta
         else:
             posLst = Player.getActions(currentRound, board, 'position', peer)
             posLstLen = len(posLst)
-            if posLstLen > 10:
-                depth -= 1
-            elif posLstLen < 4:
-                if inc < 3:
+            if posLstLen < 6:
+                if inc < 4:
                     depth += 1
                     inc += 1
             for pos in posLst:
@@ -93,7 +94,7 @@ class Player:
                     beta = curScore
                     if alpha >= beta:
                         return alpha
-                if flag and (curScore > alpha):
+                elif flag and (curScore > alpha):
                     alpha = curScore
                     if alpha >= beta:
                         return beta
@@ -111,9 +112,7 @@ class Player:
             phase = 2 if self.isFirst else 3
             if not self.isFirst:
                 currentRound += 1
-            if currentRound < 20:
-                depth = 0
-            elif currentRound < 60:
+            if currentRound < 40:
                 depth = 1
             for d in actions:
                 newBoard = board.copy()
@@ -125,8 +124,6 @@ class Player:
         else:
             phase = 0 if self.isFirst else 1
             if len(actions) > 12:
-                depth = 0
-            elif len(actions) > 8:
                 depth = 1
             for pos in actions:
                 newBoard = board.copy()
@@ -139,3 +136,8 @@ class Player:
 
     def output(self, currentRound, board, mode):
         return self.minMaxDecision(board, currentRound, mode)
+
+
+# if __name__ == '__main__':
+#     a = Player(True, [])
+#     print(a.weightSum([1, 4]))
