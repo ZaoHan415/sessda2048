e36c11f29ed82bc6c05c2fda8c050282f42f93f5
wzy-30 <57800110+wzy-30@users.noreply.github.com>
2020-05-19 14:42:45 +0800

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,4 +1,4 @@
 import sys
-SESSDA_SRC_PATH = r'C:\Users\hanze\Desktop\Doing\sessdsa.2048\src\tools\\'
+SESSDA_SRC_PATH = r'/Users/wangzhiyu/Documents/GitHub/sessdsa.2048/src/tools'
 # SESSDA_SRC_PATH = r'/mnt/c/Users/hanze/Desktop/Doing/sessdsa.2048/src'
 sys.path.append(SESSDA_SRC_PATH)
