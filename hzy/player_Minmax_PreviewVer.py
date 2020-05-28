7dfb333bb59755f2b500b52d8ad64d99758d78ea
ZaoHan415 <hanzeyao415@gmail.com>
2020-05-28 10:33:21 +0800

    Update player_Minmax_PreviewVer.py

--- hzy/player_Minmax_PreviewVer.py
+++ hzy/player_Minmax_PreviewVer.py
@@ -1,6 +1,7 @@
 # 基础版minmax 算法
 # 创建于2020年5月8日 by Czarja
 # 添加αβ剪枝，2020 5 09 by DXLi
+# 动态深度
 import numpy
 
 
@@ -10,13 +11,14 @@ class Player:
         self.isFirst = isFirst
         self.array = array
         self.maxValue = 2E9
-        # 各种奇怪的权重函数
         # self.weight = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+        # self.weight = [1, 2, 4, 8, 16, 32, 64, 128, 259, 519, 1039, 2079, 4159]
         self.weight = [1, 2, 4, 8, 16, 32, 64, 128, 259, 519, 1039, 2079, 4159]
-        # self.weight = [1, 2, 5, 11, 23, 47, 95, 191, 383, 767, 1535, 3071, 6143]
+        # self.weight = [2**i for i in range(13)]
         # self.weight = [1, 3, 7, 15, 31, 63, 127, 255, 511, 1023, 2047, 4095, 8191]
         self.maxRounds = len(array)
-        self.totalTime = 5
+        self.totalTime = 5.0
+        self.threshold = self.maxRounds * 0.15  # 开启时间控制的阈值
 
     @staticmethod
     def cannotMove(belong, board):  # 如果某方无路可走返回True
@@ -32,30 +34,41 @@ class Player:
             ownSidePos = board.getNext(peer, currentRound)
             otherSidePos = board.getNone(not peer)
             if len(ownSidePos) == 2:
-                otherSidePos.append(ownSidePos)
+                otherSidePos.insert(0, ownSidePos)
             return otherSidePos
         else:
             return [0, 1, 2, 3]
 
-    # 预测自己会不会比对方先超时，目前还不太好用
-    def gonnaOutOfTime(self, currentRound, board):
-        tLeftMe = board.getTime(self.isFirst)
-        tLeftRival = board.getTime(not self.isFirst)
-        if tLeftMe < tLeftRival:
-            return (self.totalTime - tLeftMe) * self.maxRounds / currentRound > self.totalTime
-        return False
+    def getDepth(self, currentRound, board):  # 动态调深度不太好，现在在搜索开始前把深度定死
+        depth = 9
+        if currentRound > self.threshold:  # 更新了，0.6待调整
+            tLeft = board.getTime(self.isFirst)
+            tEsti = (self.totalTime - tLeft) / float(currentRound)
+            if tEsti > 0.0105:
+                depth -= 3
+            elif tEsti > 0.0095:
+                depth -= 2
+            elif tEsti < 0.005:
+                depth += 6
+            elif tEsti < 0.006:
+                depth += 5
+            elif tEsti < 0.007:
+                depth += 4
+            elif tEsti < 0.009:
+                depth += 3
+        return depth
 
-    def weightSum(self, lst):  # 求和作为总分
-        # 换成这种列表推导式后速度快了三倍
+    def weightSum(self, lst):  # 平方和作为总分
         return sum(map(self.weight.__getitem__, lst))
 
     def score(self, board):  # 最简单的局面估值函数
         # 目前完全是玄学估值
         myScoreLst = board.getScore(self.isFirst)
         RivalScoreLst = board.getScore(not self.isFirst)
-        total = self.weightSum(myScoreLst) - self.weightSum(RivalScoreLst)/2
+        total = self.weightSum(myScoreLst) - self.weightSum(RivalScoreLst)
+        total += (len(board.getNone(self.isFirst)) - len(board.getNone(not self.isFirst))) * 2
         # 一个空位价值1.5分
-        total += len(board.getNone(self.isFirst))*1.5
+        # total += len(board.getNone(self.isFirst))*1.5
         # 无路可走的情况要避免
         # if Player.cannotMove(self.isFirst, board):
         #     return -self.maxValue
@@ -64,19 +77,32 @@ class Player:
         # else:
         return total
 
+    def scoreMove(self, board):
+        myScoreLst = board.getScore(self.isFirst)
+        RivalScoreLst = board.getScore(not self.isFirst)
+        total = self.weightSum(myScoreLst) - self.weightSum(RivalScoreLst)
+        total += (len(board.getNone(self.isFirst)) - len(board.getNone(not self.isFirst))) * 2
+        return total
+
     def _minMaxRecur(self, board, depth, phase, currentRound, inc, alpha=-(2E9), beta=+(2E9)):
         # 返回值为局面估值
         if depth <= 0 or currentRound >= self.maxRounds:
+            # if phase == 1 or phase == 2:
+            #     return self.score(board)
+            # elif phase == 3 or phase == 0:
+            #     return self.scoreMove(board)
             return self.score(board)
         peer = not bool(phase % 2)
         flag = bool(peer == self.isFirst)
+        # flag = peer
+        # flag = True时为自己回合，peer = True的时候为先手方回合
         if phase == 2 or phase == 3:
             if phase == 3:
                 currentRound += 1
             for d in [0, 1, 2, 3]:
                 newBoard = board.copy()
                 if newBoard.move(peer, d):
-                    curScore = self._minMaxRecur(newBoard, depth-1, (phase + 1) % 4, currentRound, inc, alpha, beta)
+                    curScore = self._minMaxRecur(newBoard, depth - 1, (phase + 1) % 4, currentRound, inc, alpha, beta)
                     if not flag and (curScore < beta):
                         beta = curScore
                         if alpha >= beta:
@@ -87,41 +113,40 @@ class Player:
                             return beta
         else:
             posLst = Player.getActions(currentRound, board, 'position', peer)
-            if currentRound > 100:
-                if self.gonnaOutOfTime(currentRound, board):
-                    # 如果快超时了就少搜一层
-                    depth -= 1
-                elif inc < 4:
-                    depth += 1
-                    inc += 1
-            for pos in posLst:
+            posLstLen = len(posLst)
+            if posLstLen < 5:
+                # if inc < 4:
+                #     depth += 1
+                #     inc += 1
+                for pos in posLst:
+                    newBoard = board.copy()
+                    newBoard.add(peer, pos)
+                    curScore = self._minMaxRecur(newBoard, depth-1, phase + 1, currentRound, inc, alpha, beta)
+                    if not flag and (curScore < beta):
+                        beta = curScore
+                        if alpha >= beta:
+                            return alpha
+                    elif flag and (curScore > alpha):
+                        alpha = curScore
+                        if alpha >= beta:
+                            return beta
+            else:
                 newBoard = board.copy()
-                newBoard.add(peer, pos)
+                newBoard.add(peer, posLst[0])
                 curScore = self._minMaxRecur(newBoard, depth-1, phase + 1, currentRound, inc, alpha, beta)
-                if not flag and (curScore < beta):
-                    beta = curScore
-                    if alpha >= beta:
-                        return alpha
-                elif flag and (curScore > alpha):
-                    alpha = curScore
-                    if alpha >= beta:
-                        return beta
+                return curScore
         return alpha if flag else beta
 
-    def minMaxDecision(self, board, currentRound, mode):  # 返回值为要进行的操作
+    def minmaxDecisionF(self, board, currentRound, mode):  # 针对先手进攻局
         actions = Player.getActions(currentRound, board, mode, self.isFirst)
         choice = None
         finalScore = -self.maxValue
-        depth = 2
         if mode[0] == '_':
             return None
         if mode == 'direction':
             # phase 0 1 2 3
-            phase = 2 if self.isFirst else 3
-            if not self.isFirst:
-                currentRound += 1
-            if currentRound < 10:
-                depth = 1
+            phase = 2
+            depth = self.getDepth(currentRound, board)
             for d in actions:
                 newBoard = board.copy()
                 if newBoard.move(self.isFirst, d):
@@ -130,20 +155,57 @@ class Player:
                         finalScore = curScore
                         choice = d
         else:
-            phase = 0 if self.isFirst else 1
-            if len(actions) > 14:
-                depth = 1
-            for pos in actions:
+            phase = 0
+            if len(actions) > 5 and board.getNext(self.isFirst, currentRound):  # 待调整
+                return board.getNext(self.isFirst, currentRound)
+            else:
+                depth = self.getDepth(currentRound, board)
+                for pos in actions:
+                    newBoard = board.copy()
+                    newBoard.add(self.isFirst, pos)
+                    curScore = self._minMaxRecur(newBoard, depth, phase + 1, currentRound, 0)
+                    if curScore >= finalScore:
+                        finalScore = curScore
+                        choice = pos
+        return choice
+
+    def minmaxDecisionNF(self, board, currentRound, mode):
+        actions = Player.getActions(currentRound, board, mode, self.isFirst)
+        choice = None
+        finalScore = -self.maxValue
+        if mode[0] == '_':
+            return None
+        if mode == 'direction':
+            # phase 0 1 2 3
+            phase = 3
+            depth = self.getDepth(currentRound, board)
+            for d in actions:
                 newBoard = board.copy()
-                newBoard.add(self.isFirst, pos)
-                curScore = self._minMaxRecur(newBoard, depth, phase+1, currentRound, 0)
-                if curScore >= finalScore:
-                    finalScore = curScore
-                    choice = pos
+                if newBoard.move(self.isFirst, d):
+                    curScore = self._minMaxRecur(newBoard, depth, (phase + 1) % 4, currentRound + 1, 1)
+                    if curScore >= finalScore:  # 所有情况搞一个列表，选出分值最高的
+                        finalScore = curScore
+                        choice = d
+        else:
+            phase = 1
+            if len(actions) > 5 and board.getNext(self.isFirst, currentRound):  # 待调整
+                return board.getNext(self.isFirst, currentRound)
+            else:
+                depth = self.getDepth(currentRound, board)
+                for pos in actions:
+                    newBoard = board.copy()
+                    newBoard.add(self.isFirst, pos)
+                    curScore = self._minMaxRecur(newBoard, depth, phase + 1, currentRound, 0)
+                    if curScore >= finalScore:
+                        finalScore = curScore
+                        choice = pos
         return choice
 
     def output(self, currentRound, board, mode):
-        return self.minMaxDecision(board, currentRound, mode)
+        if self.isFirst:
+            return self.minmaxDecisionF(board, currentRound, mode)
+        else:
+            return self.minmaxDecisionNF(board, currentRound, mode)
 
 
 # if __name__ == '__main__':
